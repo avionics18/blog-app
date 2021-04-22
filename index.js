42c6da9fa0d8502f7a201b302aa16eb688d4fe84
Avinash Murmu <avimurmu18@gmail.com>
2021-04-22 17:14:23 +0530

    ready for deployment

--- index.js
+++ index.js
@@ -119,6 +119,7 @@ app.delete("/blog/:id", (req, res) => {
 });
 
 // server
-app.listen(3000, () => {
+const port = process.env.PORT || 3000;
+app.listen(port, () => {
   console.log("Server running at port 3000!");
 });
